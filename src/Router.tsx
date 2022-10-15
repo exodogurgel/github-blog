59e47d1b9841a2552f042d6715f97538a38b0e55
Êxodo Gurgel <exodowellis@gmail.com>
2022-10-15 18:12:01 -0300

    :sparkles: feat: Create route layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 import { Home } from './pages/Home'
 import { Post } from './pages/Post'
@@ -6,8 +7,10 @@ import { Post } from './pages/Post'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/post/:id" element={<Post />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<Post />} />
+      </Route>
     </Routes>
   )
 }
