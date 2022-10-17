b63a9889f5363d135bd6dfb261eed6b809ca82c4
Êxodo Gurgel <exodowellis@gmail.com>
2022-10-17 15:46:25 -0300

    :sparkles: feat: Create Search section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,17 @@
 import { Profile } from './components/Profile'
-import { HomeContainer } from './styles'
+import { HomeContainer, SearchSection } from './styles'
 
 export function Home() {
   return (
     <HomeContainer>
       <Profile />
+      <SearchSection>
+        <div>
+          <h2>Publicações</h2>
+          <span>6 publicações</span>
+        </div>
+        <input type="text" placeholder="Buscar conteúdo" />
+      </SearchSection>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -12,3 +12,39 @@ export const HomeContainer = styled.div`
     padding-inline: 1.5rem;
   }
 `
+
+export const SearchSection = styled.section`
+  margin-top: 4.375rem;
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+
+    h2 {
+      font-size: 1.125rem;
+      line-height: 1.6;
+      color: ${(props) => props.theme['blue-100']};
+    }
+
+    span {
+      font-size: 0.875rem;
+      color: ${(props) => props.theme['blue-300']};
+    }
+  }
+
+  input {
+    width: 100%;
+    height: 3.125rem;
+    border: 1px solid ${(props) => props.theme['blue-500']};
+    border-radius: 6px;
+    padding: 0.75rem 1rem;
+    background: ${(props) => props.theme['blue-900']};
+    color: ${(props) => props.theme['blue-200']};
+
+    &::placeholder {
+      color: ${(props) => props.theme['blue-400']};
+    }
+  }
+`
