e5e0b552572c72ddc6411ab1e611ed8def746f6c
Êxodo Gurgel <exodowellis@gmail.com>
2022-10-18 10:25:33 -0300

    :sparkles: feat: Fetch data from github profile

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -3,25 +3,68 @@ import { ProfileContainer, ProfileContent } from './styles'
 
 import profileImg from '../../../../assets/profile.jpeg'
 import { GitLink } from '../../../../components/GitLink'
+import { useEffect, useState } from 'react'
+import { api } from '../../../../lib/axios'
+
+interface ProfileProps {
+  avatarURL: string
+  bio: string
+  htmlURL: string
+  followers: number
+  login: string
+  name: string
+}
 
 export function Profile() {
+  const [profile, setProfile] = useState<ProfileProps>({
+    avatarURL: '',
+    bio: '',
+    htmlURL: '',
+    followers: 1,
+    login: '',
+    name: '',
+  })
+
+  async function fetchProfile() {
+    const response = await api.get('/users/exodogurgel')
+
+    const {
+      avatar_url: avatarURL,
+      bio,
+      html_url: htmlURL,
+      followers,
+      login,
+      name,
+    } = response.data
+
+    const filteredData = {
+      avatarURL,
+      bio,
+      htmlURL,
+      followers,
+      login,
+      name,
+    }
+
+    setProfile(filteredData)
+  }
+
+  useEffect(() => {
+    fetchProfile()
+  }, [])
   return (
     <ProfileContainer>
       <img src={profileImg} alt="" />
       <ProfileContent>
         <header>
-          <h2>Êxodo Gurgel</h2>
-          <GitLink link="https://www.github.com/exodogurgel" title="Github" />
+          <h2>{profile.name}</h2>
+          <GitLink link={profile.htmlURL} title="Github" />
         </header>
-        <p>
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
-          pulvinar vel mass.
-        </p>
+        <p>{profile.bio}</p>
         <footer>
           <span>
             <FaGithub />
-            exodogurgel
+            {profile.login}
           </span>
           <span>
             <FaBuilding />
@@ -29,7 +72,7 @@ export function Profile() {
           </span>
           <span>
             <FaUserFriends />
-            32 seguidores
+            {profile.followers} seguidores
           </span>
         </footer>
       </ProfileContent>
