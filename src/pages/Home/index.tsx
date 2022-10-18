33d1e94764c21bbf39016a290f44be9f780f2865
Êxodo Gurgel <exodowellis@gmail.com>
2022-10-18 11:28:05 -0300

    :sparkles: feat: Apply react-hook-form in the search

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,24 @@
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import { PostCard } from './components/PostCard'
 import { Profile } from './components/Profile'
 import { HomeContainer, Publications, SearchSection } from './styles'
 
+const searchInputSchema = z.object({
+  query: z.string(),
+})
+
+type SearchInput = z.infer<typeof searchInputSchema>
+
 export function Home() {
+  const { register, watch } = useForm<SearchInput>({
+    resolver: zodResolver(searchInputSchema),
+  })
+
+  console.log(watch('query'))
+
   return (
     <HomeContainer>
       <Profile />
@@ -11,7 +27,11 @@ export function Home() {
           <h2>Publicações</h2>
           <span>6 publicações</span>
         </div>
-        <input type="text" placeholder="Buscar conteúdo" />
+        <input
+          type="text"
+          placeholder="Buscar conteúdo"
+          {...register('query')}
+        />
       </SearchSection>
       <Publications>
         <PostCard />
